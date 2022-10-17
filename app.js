29c87005fa65a5bcdd76fd44a30e512ffc9209be
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2022-10-17 07:38:41 +0100

    updating

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+$(document).ready(function () {
+  $("#testimonial-slider").owlCarousel({
+    items: 3,
+    itemsDesktop: [1000, 3],
+    itemsDesktopSmall: [980, 2],
+    itemsTablet: [768, 2],
+    itemsMobile: [650, 1],
+    pagination: true,
+    navigation: false,
+    slideSpeed: 1000,
+    autoPlay: true,
+  });
+});
 
 
+let hamburger = document.querySelector('.hamburger');
+let navMenu = document.querySelector('.navbar-links');
 
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(e => e.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove("active");
+}))
